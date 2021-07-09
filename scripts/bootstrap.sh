7caf9edef7ed62d19429eb9b25acfc815c72e9fb
jrmorgan <je.morgan@f5.com>
2021-07-09 12:32:26 -0400

    Add full example for client auth

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,37 +1,71 @@
 #! /bin/ash
 
-if test ! -f "/bootstrap/bootstrap"; then
-  #lerna bootstrap
+DIR=/ca
+CDIR=${DIR}/clientcert
 
-  # This provides an init-contianer-like experience on docker-compose, which
-  # doesn't natively support init containers.
-  today=$(date +"%Y-%m-%d")
+mkdir -p ${CDIR}
+microdnf -y install openssl
 
-  echo "${today}" > /bootstrap/bootstrap
+if [ ! -f ${CDIR}/client.pem ]; then
 
   # Generate private key and cert for CA:
   openssl req -x509 -new -newkey rsa:4096 -nodes \
     -subj "/C=US/ST=FL/L=Boreville/O=Dis/CN=ca.shifti.us" \
     -days 1825 \
-    -keyout /ca/ca-ex.key \
-    -out /ca/ca-ex.pem
+    -keyout ${DIR}/ca-ex.key \
+    -out ${DIR}/ca-ex.pem
 
-  # Create client subdir
-  mkdir -p /ca/clientcert
+  # Generate openssl conf
+  cat <<EOT >> ${DIR}/openssl-ext.cnf
+basicConstraints = CA:FALSE
+nsCertType = client, email
+nsComment = "OpenSSL Generated Client Certificate for NGINX CAC"
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer
+keyUsage = critical, nonRepudiation, digitalSignature, keyEncipherment
+extendedKeyUsage = clientAuth, emailProtection
+EOT
 
   # Generate a sample client certificate:
 
   ### Key and CSR
-  openssl x509 -req -new -newkey rsa:4096 -nodes \
+  openssl genrsa -out ${CDIR}/client.key 4096
+  openssl req -new \
+    -key ${CDIR}/client.key \
     -subj "/C=US/ST=FL/L=Boreville/O=Dis/CN=client.shifti.us/emailAddress=client@shifti.us" \
+    -out ${CDIR}/client.csr
+
+  ### Sign CSR with CA cert/key
+  openssl x509 -req -in ${CDIR}/client.csr -CA ${DIR}/ca-ex.pem -CAkey ${DIR}/ca-ex.key \
+    -out ${CDIR}/client.pem \
+    -CAcreateserial \
     -days 365 \
-    -extfile /ca/client_cert_ext.cnf \
-    -CA /ca/ca-ex.pem \
-    -CAkey /ca/ca-ex.key \
-    -keyout /ca/client-ex.key \
-    -out /ca/client-ex.pem
+    -sha256 \
+    -extfile ${DIR}/openssl-ext.cnf
 
-
+  ### Generate a PKCS12 file for easy import
+  openssl pkcs12 -export -inkey ${CDIR}/client.key \
+    -in ${CDIR}/client.pem \
+    -passout pass:changeme \
+    -name NGINX_Client_Certificate \
+    -out ${CDIR}/client.pfx
 fi
+
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+echo -e "Your CA certificate and key is located in:              ${DIR}"
+echo -e "Your signed client certificate and key is located in:   ${CDIR}"
+echo -e "${RED}Attempt to access NGINX with client certificate auth enabled at: ${GREEN}http://localhost:8443"
+echo -e "${RED}You should receive response 400 - 'No Required SSL Certificate sent'\n"
+
+echo -e "${RED}Quick test WITHOUT MUTUAL TLS (Returns 400):"
+echo -e "${NC}curl --cacert ./ca/ca-ex.pem --resolve ca.shifti.us:8443:127.0.0.1 https://ca.shifti.us:8443\n"
+echo -e "${GREEN}Quick test WITH MUTUAL TLS (Returns 200):"
+echo -e "${NC}curl --cert ./ca/clientcert/client.crt --key ./ca/clientcert/client.key --cacert ./ca/ca-ex.pem https://127.0.0.1:8443\n"
+
+echo -e "After testing rejection based on a missing client certificate, import the generated PKCS12 cert into your "
 
 sleep infinity
